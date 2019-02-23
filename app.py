89b9146642feed9761790af1a85dcdb24c5b498b
19Pablo19 <p.alcazar04@gmail.com>
2019-02-23 11:59:26 +0100

    login call

--- app.py
+++ app.py
@@ -2,10 +2,30 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+users = [{"Pilar": "123"},
+         {"Marcos": "123"}]
 
-@app.route('/<name>')
-def index(name):
-    return render_template('index.html', name=name)
+# Example for the login
+# for user in range(len(users)):
+#     if "Pilar" in users[user]:
+#         print("SIUUH")
+#     else:
+#         print("NOP")
+
+
+questions = [{"": ""},
+             {"": ""}]
+
+
+
+@app.route('/login', methods=['POST'])
+def index(name, password):
+    for user in range(len(users)):
+        if name in users[user] and users[user][name]:
+            return render_template('index.html', user=name, password=passsword)
+        else:
+            return print("User or password incorrect")
+
 
 
 @app.route("/echo", methods=['POST'])
