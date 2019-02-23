a137b2a6a82bacc8f6db2f672ae3d5df66a44e82
19Pablo19 <p.alcazar04@gmail.com>
2019-02-24 00:54:36 +0100

    changes

--- templates/reports.py
+++ templates/reports.py
@@ -11,25 +11,27 @@
 		left:50%;
 		width:400px;
 		margin-left:-200px;
-		height:300px;
-		margin-top:-150px;
+		margin-top:-190px;
 		padding:5px;
+        text-align:center;
+        background-color: #e6fff7;
+        padding-top:15px;
 	}
   </style>
   </head>
   <body>
     <div class="centrar">
     <form action="{{ url_for('report') }}" method="POST">
-  <label for="respuesta1">Respuesta 1</label><p>Respuesta</p>
-  <label for="respuesta2">Respuesta 2</label><p>Respuesta</p>
-  <label for="respuesta3">Respuesta 3</label><p>Respuesta</p>
-  <label for="respuesta4">Respuesta 4</label><p>Respuesta</p>
-  <label for="respuesta5">Respuesta 5</label><p>Respuesta</p>
-  <label for="respuesta6">Respuesta 6</label><p>Respuesta</p>
+  <label for="respuesta1">¿Dónde ha sucedido?</label><p>Recinto escolar y/o alrededores</p><br>
+  <label for="respuesta2">Suceso:</label><p>Pelea</p><br>
+  <label for="respuesta3">¿Quiénes han participado?</label><p>Varios alumnos (=2)</p><br>
+  <label for="respuesta4">Resultado:</label><p>Arañazos</p><br>
+  <label for="respuesta5">¿Ha sido necesaria asistencia médica?</label><p>Sí</p><br>
+  <label for="respuesta6">Esa asistencia ha sido</label><p>Interna del propio centro</p><br>
 
 
 
-<input type="submit" value="Echo" class="button is-primary" id="button">
+<input type="submit" value="Enviar" class="button is-primary" id="button" style="margin-top: 10px;">
 </form>
 </div>
   </body>
